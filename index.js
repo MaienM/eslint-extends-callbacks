24d4839c8478226e78a43b25c0225b9a126d725a
Michon van Dooren <michon1992@gmail.com>
2017-11-20 10:47:11 +0100

    Removed the (undeclared) dependency on lodash

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 const Config = require('eslint/lib/config');
 const ConfigFile = require('eslint/lib/config/config-file');
 const Linter = require('eslint/lib/linter');
-const _ = require('lodash');
 
 function extendsCallbacks(config) {
 	// Expand the config, minus the rules
 	const expanded = ConfigFile.applyExtends(
-		_.omit(config, 'rules'),
+		Object.assign({}, config, { rules: [] }),
 		new Config({}, new Linter()),
 		require.resolve('eslint-config-airbnb'),
 		__dirname,
 	);
 
 	// Look for any rules that are callbacks and invoke them
-	const expandedRules = _.mapValues(config.rules, (value, key) => {
-		if (!_.isFunction(value)) {
+	const expandedRules = Object.entries(config.rules).map(function(entry) {
+		const key = entry[0], value = entry[1];
+		if (typeof value !== 'function') {
 			return value;
 		}
-
 		return value.apply(value, expanded.rules[key]);
 	});
 
 	// Merge with base config and return
-	return _.assign({}, config, { rules: expandedRules });
+	return Object.assign({}, config, { rules: expandedRules });
 }
 
 module.exports = extendsCallbacks;
